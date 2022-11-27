0e63024f88a109e31abbbe499baf213340ebe6dc
Jay Sprout <jotasprout@gmail.com>
2022-11-27 14:12:11 -0500

    removed obsolete comments

--- AbilityRoll.java
+++ AbilityRoll.java
@@ -1,74 +1,74 @@
 import java.util.Random;
 import java.util.HashMap;
 
-public class AbilityRoll 
-{
-    //HashMap<String, String> abilityRollScores = new HashMap<String, String>();
+public class AbilityRoll {
+        // HashMap<String, String> abilityRollScores = new HashMap<String, String>();
 
-    public int sixSidedRoll;
-    public int abilityRoll;
+        public int sixSidedRoll;
+        public int abilityRoll;
 
-    public AbilityRoll(Character myChar)
-    {
-       String[] abilities = {"strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"};
+        public AbilityRoll(Character myChar) {
+                String[] abilities = { "strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma" };
 
-       HashMap<String, Integer> abilityRollScores = new HashMap<String, Integer>();
+                HashMap<String, Integer> abilityRollScores = new HashMap<String, Integer>();
 
-       for (String ability : abilities)
-       {
-               System.out.println("Rolling for " + ability + " ...");
+                for (String ability : abilities) {
+                        System.out.println("Rolling for " + ability + " ...");
 
-               switch(ability) {
-                       case "strength": System.out.println(ability + " is " + abilityRoll + ".");
-                               myChar.strength = abilityRoll;
-                               abilityRollScores.put("strength", myChar.strength);
-                               break;
-                       case "dexterity": System.out.println(ability + " is " + abilityRoll + ".");
-                               myChar.dexterity = abilityRoll;
-                               abilityRollScores.put("dexterity", myChar.dexterity);
-                               break;
-                       case "constitution": System.out.println(ability + " is " + abilityRoll + ".");
-                               myChar.constitution = abilityRoll;
-                               abilityRollScores.put("constitution", myChar.constitution);
-                               break;
-                       case "intelligence": System.out.println(ability + " is " + abilityRoll + ".");
-                               myChar.intelligence = abilityRoll;
-                               abilityRollScores.put("intelligence", myChar.intelligence);
-                               break;  
-                       case "wisdom":System.out.println(ability + " is " + abilityRoll + ".");
-                               myChar.wisdom = abilityRoll;
-                               abilityRollScores.put("wisdom", myChar.wisdom);
-                               break;
-                       case "charisma": System.out.println(ability + " is " + abilityRoll + ".");
-                               myChar.charisma = abilityRoll;
-                               abilityRollScores.put("charisma", myChar.charisma);
-                               break;                   
-                       default: System.out.println("You need better dice.");
-                       // need a custom exception here
-               }   
-       }
-       System.out.println(abilityRollScores);
-    }
-
-    // roll one six-sided die
-    public int rollSixSided()
-    {
-        Random objGenerator = new Random();
-        int randomSixSidedNum = objGenerator.nextInt(6);
-        return randomSixSidedNum;
-    }
-
-    // roll for abilities
-    // Simulation of rolling three six-sided die for each ability
-    public int rollForAbility()
-    {
-        for (int die = 0;  die <= 2; die++){
-            sixSidedRoll = rollSixSided();
-            abilityRoll += sixSidedRoll;
+                        switch (ability) {
+                                case "strength":
+                                        System.out.println(ability + " is " + abilityRoll + ".");
+                                        myChar.strength = abilityRoll;
+                                        abilityRollScores.put("strength", myChar.strength);
+                                        break;
+                                case "dexterity":
+                                        System.out.println(ability + " is " + abilityRoll + ".");
+                                        myChar.dexterity = abilityRoll;
+                                        abilityRollScores.put("dexterity", myChar.dexterity);
+                                        break;
+                                case "constitution":
+                                        System.out.println(ability + " is " + abilityRoll + ".");
+                                        myChar.constitution = abilityRoll;
+                                        abilityRollScores.put("constitution", myChar.constitution);
+                                        break;
+                                case "intelligence":
+                                        System.out.println(ability + " is " + abilityRoll + ".");
+                                        myChar.intelligence = abilityRoll;
+                                        abilityRollScores.put("intelligence", myChar.intelligence);
+                                        break;
+                                case "wisdom":
+                                        System.out.println(ability + " is " + abilityRoll + ".");
+                                        myChar.wisdom = abilityRoll;
+                                        abilityRollScores.put("wisdom", myChar.wisdom);
+                                        break;
+                                case "charisma":
+                                        System.out.println(ability + " is " + abilityRoll + ".");
+                                        myChar.charisma = abilityRoll;
+                                        abilityRollScores.put("charisma", myChar.charisma);
+                                        break;
+                                default:
+                                        System.out.println("You need better dice.");
+                                        // need a custom exception here
+                        }
+                }
+                System.out.println(abilityRollScores);
         }
-        return abilityRoll;
-    } 
 
+        // roll one six-sided die
+        public int rollSixSided() {
+                Random objGenerator = new Random();
+                int randomSixSidedNum = objGenerator.nextInt(6);
+                return randomSixSidedNum;
+        }
 
+        // roll for abilities
+        // Simulation of rolling three six-sided die for each ability
+        public int rollForAbility() {
+                for (int die = 0; die <= 2; die++) {
+                        sixSidedRoll = rollSixSided();
+                        abilityRoll += sixSidedRoll;
+                }
+                return abilityRoll;
+        }
 
 }
--- CharCreate.java
+++ CharCreate.java
@@ -2,27 +2,24 @@ import java.util.*;
 
 public class CharCreate {
 
-    public CharCreate(){
+    public CharCreate() {
         System.out.println("Are you old like me and tried to teach your children Dungeons & Dragons?");
         System.out.println("Felt even more stupid than when you try to help with their homework, right?");
-        System.out.println("This app does all those newfangled calculations -- that make the game both more realistic but also more of a pain in the ass -- for you.");
-        System.out.println("I'll sidestep the bummer that you don't get to roll the dice by saying, even if you didn't buy or lost your dice, the app takes care of that, too.");
+        System.out.println(
+                "This app does all those newfangled calculations -- that make the game both more realistic but also more of a pain in the buttocks -- for you.");
+        System.out.println(
+                "I'll sidestep the bummer that you don't get to roll the dice by saying, even if you didn't buy or lost your dice, the app takes care of that, too.");
 
         // SCANNER CREATION
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("We create your character by naming it -- you can always change the name later if you feel Dwarf parents would't really name their daughter Bernie.");
+        System.out.println(
+                "We create your character by naming it -- you can always change the name later if you feel Dwarf parents would't really name their daughter Bernie.");
         System.out.println("What is your character's name?");
         String myName = scan.nextLine();
         Character newChar = new Character(myName);
 
-        // HOW DO I GET THE CHARACTER I JUST CREATED TO USE THE AbilityRoll METHOD (or the other way around)
-        // so the abilities get added to the character?
-        // It works if I make it spaghetti code and spread it over several classes ... 
-
-
-        scan.close();        
+        scan.close();
     }
-
 
 }
