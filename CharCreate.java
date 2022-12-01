271e3174ad7c32a7986e3a6a88374047fc87cdd9
Jay Sprout <jotasprout@gmail.com>
2022-12-01 12:20:02 -0500

    Add some formatting cuz I'm a little obsessive.

--- CharCreate.java
+++ CharCreate.java
@@ -2,22 +2,29 @@ import java.util.*;
 
 public class CharCreate {
 
-    public CharCreate() {
-        System.out.println("Are you old like me and tried to teach your children Dungeons & Dragons?");
-        System.out.println("Felt even more stupid than when you try to help with their homework, right?");
+    public CharCreate() 
+    {
+
+        System.out.println("Are you old like me and tried to teach your children Dungeons & Dragons?\n");
+
+        System.out.println("Felt even more stupid than when you try to help with their homework, right?\n");
+
         System.out.println(
-                "This app does all those newfangled calculations -- that make the game both more realistic but also more of a pain in the buttocks -- for you.");
+                "This app does all those newfangled calculations -- that make the game both more realistic but also more of a pain in the buttocks -- for you.\n");
+
         System.out.println(
-                "I'll sidestep the bummer that you don't get to roll the dice by saying, even if you didn't buy or lost your dice, the app takes care of that, too.");
+                "I'll sidestep the bummer that you don't get to roll the dice by saying, even if you didn't buy or lost your dice, the app takes care of that, too.\n");
 
         // SCANNER CREATION
         Scanner scan = new Scanner(System.in);
 
         System.out.println(
-                "We create your character by naming it -- you can always change the name later if you feel Dwarf parents would't really name their daughter Bernie.");
-        System.out.println("What is your character's name?");
-        String myName = scan.nextLine();
-        Character newChar = new Character(myName);
+                "We create your character by naming it -- you can always change the name later if you feel Dwarf parents would't really name their daughter Bernie.\n");
+
+        System.out.println("What is your character's name?\n");
+
+        //String myName = scan.nextLine();
+        //Character newChar = new Character(myName);
 
         scan.close();
     }
